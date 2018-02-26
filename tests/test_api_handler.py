3211c6dff7d95b37ef42f49919267184ccaa7119
MHajoha <MHajoha@gmail.com>
2018-02-27 00:22:51 +0100

    Rework the tests (again)

--- tests/test_api_handler.py
+++ tests/test_api_handler.py
@@ -1,17 +1,49 @@
 import unittest
 
+import asyncio
 from aiounittest import async_test
 
 from Modules.api_handler import WebsocketAPIHandler20, WebsocketAPIHandler21, MismatchedVersionError
 
+hostname_v20 = "api.fuelrats.com"
+hostname_v21 = "dev.api.fuelrats.com"
 
-class APIHandlerTest(unittest.TestCase):
-    hostname_v20 = "api.fuelrats.com"
-    hostname_v21 = "dev.api.fuelrats.com"
 
+class CommonAPIHandlerTest(unittest.TestCase):
+    """Tests spanning across multiple versions."""
     @async_test
+    async def test_version(self):
+        handler20 = WebsocketAPIHandler20(hostname=hostname_v21)
+        handler21 = WebsocketAPIHandler21(hostname=hostname_v20)
+
+        with self.subTest("handler < api"):
+            with self.assertRaises(MismatchedVersionError):
+                await handler20.connect()
+
+        with self.subTest("handler > api"):
+            with self.assertRaises(MismatchedVersionError):
+                await handler21.connect()
+
+        if handler20.connected:
+            await handler20.disconnect()
+        if handler21.connected:
+            await handler21.disconnect()
+
+
+class APIHandlerTest20(unittest.TestCase):
+    """Test case for v2.0."""
+    hostname = hostname_v20
+    handler_class = WebsocketAPIHandler20
+
+    @classmethod
+    def setUpClass(cls):
+        # We need to decorate in here because aiounittest doesn't seem to like inheritance
+        cls.test_connection = async_test(cls.test_connection, asyncio.get_event_loop())
+        cls.test_call = async_test(cls.test_call, asyncio.get_event_loop())
+
     async def test_connection(self):
-        handler = WebsocketAPIHandler21(hostname=self.hostname_v21)
+        """Test that the connect, reconnect and disconnect methods work as intended."""
+        handler = self.handler_class(hostname=self.hostname)
 
         with self.subTest("connect"):
             await handler.connect()
@@ -28,27 +60,9 @@ class APIHandlerTest(unittest.TestCase):
             self.assertFalse(handler.connected)
             self.assertTrue(handler._listener_task.done())
 
-    @async_test
-    async def test_version(self):
-        handler20 = WebsocketAPIHandler20(hostname=self.hostname_v21)
-        handler21 = WebsocketAPIHandler21(hostname=self.hostname_v20)
-
-        with self.subTest("handler < api"):
-            with self.assertRaises(MismatchedVersionError):
-                await handler20.connect()
-
-        with self.subTest("handler > api"):
-            with self.assertRaises(MismatchedVersionError):
-                await handler21.connect()
-
-        if handler20.connected:
-            await handler20.disconnect()
-        if handler21.connected:
-            await handler21.disconnect()
-
-    @async_test
     async def test_call(self):
-        handler = WebsocketAPIHandler21(hostname=self.hostname_v21)
+        """Test the _call method as far as we can without a token."""
+        handler = self.handler_class(hostname=self.hostname)
 
         await handler.connect()
         response = await handler._call("version", "read")
@@ -56,3 +70,9 @@ class APIHandlerTest(unittest.TestCase):
         self.assertEqual(response["data"].keys(), {"id", "type", "attributes"})
         self.assertEqual(response["data"]["attributes"].keys(), {"version", "commit", "branch", "tags", "date"})
         await handler.disconnect()
+
+
+class APIHandlerTest21(APIHandlerTest20):
+    """Test case for v2.1."""
+    hostname = hostname_v21
+    handler_class = WebsocketAPIHandler21
