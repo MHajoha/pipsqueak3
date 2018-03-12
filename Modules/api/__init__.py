46f8927f8cac092ed429aa339eea764eed6d6e88
MHajoha <MHajoha@gmail.com>
2018-03-12 21:08:08 +0100

    Make get_correct_version_handler async

--- Modules/api/__init__.py
+++ Modules/api/__init__.py
@@ -1,5 +1,3 @@
-import asyncio
-
 from .exceptions import MismatchedVersionError
 from .api_handler import APIHandler
 from .v20 import WebsocketAPIHandler20
@@ -10,8 +8,7 @@ from .websocket import WebsocketRequestHandler
 _handlers = {WebsocketAPIHandler20, WebsocketAPIHandler21}
 
 
-def get_correct_version_handler(hostname: str, token: str=None, tls=True, *,
-                                loop: asyncio.BaseEventLoop=None) -> APIHandler:
+async def get_correct_version_handler(hostname: str, token: str=None, tls=True) -> APIHandler:
     """
     Get the correct handler for the API version which the hostname provided is running.
     The returned handler will already be connected.
@@ -20,19 +17,16 @@ def get_correct_version_handler(hostname: str, token: str=None, tls=True, *,
         hostname (str): Hostname to connect to.
         token (str): OAuth token to be used for authorization or None if it's not needed.
         tls (bool): Whether to use TLS when connecting or not.
-        loop (asyncio.BaseEventLoop): Event loop to run the handler on. Defaults to global loop.
 
     Raises:
         ValueError: If no appropriate handler can be found.
     """
-    if loop is None:
-        loop = asyncio.get_event_loop()
 
     for handler_class in _handlers:
-        handler: APIHandler = handler_class(hostname, token, tls, loop=loop)
+        handler: APIHandler = handler_class(hostname, token, tls)
 
         try:
-            loop.run_until_complete(handler.connect())
+            await handler.connect()
         except MismatchedVersionError:
             continue
         else:
