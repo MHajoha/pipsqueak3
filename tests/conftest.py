32694ca659eae72430646ed4c97e740098747dae
MHajoha <MHajoha@gmail.com>
2018-04-28 12:04:56 +0200

    Move handler fixture to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,6 +12,8 @@ Licensed under the BSD 3-Clause License.
 See LICENSE
 """
 import logging
+import asyncio
+import json
 import random
 import string
 import sys
@@ -19,6 +21,7 @@ from uuid import uuid4, UUID
 
 import psycopg2
 import psycopg2.pool
+import websockets
 import pytest
 
 # from psycopg2.pool import SimpleConnectionPool
@@ -40,6 +43,7 @@ setup_logging("logs/unit_tests.log")
 
 from Modules.permissions import Permission
 from tests.mock_bot import MockBot
+from Modules.api import WebsocketAPIHandler20, WebsocketAPIHandler21
 from Modules.rat_board import RatBoard
 from Modules.rat_rescue import Rescue
 from Modules.rat import Rat
@@ -317,3 +321,95 @@ def test_fact_fx() -> Fact:
                 mfd=False,
                 edited=None
                 )
+
+
+@pytest.fixture(params=[WebsocketAPIHandler20, WebsocketAPIHandler21], ids=["v2.0", "v2.1"])
+def handler_fx(request):
+    """
+    Fixture for API handler tests.
+    Replaces :fun:`websockets.connect` with a lambda returning the fake object.
+
+    Returns:
+        (APIHandler, function, function): A 3-tuple with the following content:
+            [0]: A handler instance. Not yet connected.
+            [1]: was_sent: A convenience function to test whether or not the aforementioned handler
+                sent a given json dict.
+            [2]: function: respond: A convenience function to fake a response from the server in the
+                mock websocket connection.
+
+    Example:
+        >>> async def my_test(handler_fx):
+        ...     handler, was_sent, function = handler_fx
+        ...     await handler_fx.connect()
+
+    Read below for more information.
+    """
+
+    class MockWebsocketConnection(object):
+        """Fake websocket connection object to be used with the below convenience functions."""
+        def __init__(self):
+            super().__init__()
+            self.sent_messages = []
+            self.incoming_messages = []
+
+            self.host = "some_host"
+            self.open = True
+
+        async def recv(self):
+            while len(self.incoming_messages) == 0:
+                await asyncio.sleep(0.1)
+
+            return json.dumps(self.incoming_messages.pop(0))
+
+        async def close(self, reason):
+            self.open = False
+
+    original_connect = websockets.connect
+    websockets.connect = lambda *args, **kwargs: MockWebsocketConnection()
+
+    instance = request.param("some_hostname", "some_token", "tls_or_not")
+
+    def was_sent(data: dict) -> bool:
+        """
+        Checks if the specified dict was sent (meta field will be excluded for comparison) and
+        removes it from the record if so.
+        """
+        for i, message in enumerate(instance._connection.sent_messages):
+            try:
+                message.pop("meta")
+            except KeyError:
+                pass
+
+            try:
+                data.pop("meta")
+            except KeyError:
+                pass
+
+            if data == message:
+                instance._connection.sent_messages.pop(i)
+                return True
+        else:
+            return False
+
+    def respond(data: dict):
+        """
+        Queues the provided dict as a response to a previously made request. Only works when only
+        one request is waiting for a response.
+
+        Raises:
+             RuntimeError: If the above condition is not met.
+        """
+        if len(instance._waiting_requests) != 1:
+            raise RuntimeError
+        else:
+            instance._connection.incoming_messages.append(
+                {
+                    "meta": {
+                        "request_id": next(iter(instance._waiting_requests))
+                    }
+                }.update(data)
+            )
+
+    yield instance, was_sent, respond
+
+    websockets.connect = original_connect
