a7dda6c84e22179b7cee0b0f9b6f2c1f065a665d
MHajoha <MHajoha@gmail.com>
2018-05-03 00:19:47 +0200

    Properly check handler type in test

--- tests/test_api_handler.py
+++ tests/test_api_handler.py
@@ -4,7 +4,7 @@ from uuid import UUID
 import asyncio
 import pytest
 
-from Modules.api import WebsocketAPIHandler20
+from Modules.api import WebsocketAPIHandler20, WebsocketAPIHandler21
 from Modules.rat_rescue import Rescue
 from tests.mock_connection import MockWebsocketConnection
 
@@ -26,11 +26,11 @@ async def test_get_rescues(handler_fx: Tuple[WebsocketAPIHandler20, MockWebsocke
         return_when=asyncio.FIRST_COMPLETED
     ))[0].pop().result()
 
-    if isinstance(handler, WebsocketAPIHandler20):
+    if type(handler) is WebsocketAPIHandler20:
         assert connection.was_sent({"action": ["rescues", "read"],
                                     "client": "Some Client",
                                     "firstLimpetId": "dc9c91fb-9ead-47e9-8771-81da2c1971bc"})
-    else:
+    elif type(handler) is WebsocketAPIHandler21:
         assert connection.was_sent({"action": ["rescues", "search"],
                                     "client": "Some Client",
                                     "firstLimpetId": "dc9c91fb-9ead-47e9-8771-81da2c1971bc"})
