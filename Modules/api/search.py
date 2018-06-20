6b3945a085f21f884f14ddd9ebf1341f9141288a
MHajoha <MHajoha@gmail.com>
2018-06-21 01:05:46 +0200

    Implement some sequelize operators

--- Modules/api/search.py
+++ Modules/api/search.py
@@ -8,10 +8,59 @@ Licensed under the BSD 3-Clause License.
 
 See LICENSE.md
 """
+from abc import ABC, abstractmethod
 from typing import Callable, Dict, Tuple, Any, Union, Sequence
 
 from utils.nested import set_nested
 from utils.typechecking import check_type
+
+
+class _SequelizeOperator(ABC):
+    @abstractmethod
+    def generate(self, sanitize: Callable=None) -> Union[dict, list]:
+        pass
+
+
+class _UnaryOp(_SequelizeOperator):
+    _op = None
+
+    def __init__(self, value):
+        self._value = value
+
+    def generate(self, sanitize: Callable=None) -> dict:
+        if sanitize is None:
+            return {self._op: self._value}
+        else:
+            return {self._op: sanitize(self._value)}
+
+
+class Not(_UnaryOp):
+    _op = "$not"
+
+
+class Contains(_UnaryOp):
+    _op = "$contains"
+
+
+class _SequenceOperator(_SequelizeOperator):
+    _op = None
+
+    def __init__(self, *values):
+        self._values = values
+
+    def generate(self, sanitize: Callable=None) -> dict:
+        if sanitize is None:
+            return {self._op: list(self._values)}
+        else:
+            return {self._op: [sanitize(value) for value in self._values]}
+
+
+class In(_SequenceOperator):
+    _op = "$in"
+
+
+class NotIn(_SequelizeOperator):
+    _op = "$notIn"
 
 
 class Search(object):
@@ -38,7 +87,9 @@ class Search(object):
             if types is not None:
                 check_type(value, *types)
 
-            if sanitize is None:
+            if isinstance(value, _SequelizeOperator):
+                set_nested(result, json_key, value.generate(sanitize))
+            elif sanitize is None:
                 set_nested(result, json_key, value)
             else:
                 set_nested(result, json_key, sanitize(value))
